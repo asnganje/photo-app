3cc27d546f747b0385c7352bd9ed02a96ca87a07
asnganje <abd.nganje@gmail.com>
2025-03-26 11:33:41 +0300

    add helper for bootstrap notices

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def bootstrap_class_for(flash_type)
+    case flash_type.to_sym
+    when :notice then "success"
+    when :alert then "warning"
+    when :error then "danger"
+    when :info then "info"
+    else "secondary"
+    end
+  end
 end
