18f221ece39c676d652c3df89ba21a746a4f3288
asnganje <abd.nganje@gmail.com>
2025-03-26 22:14:25 +0300

    add stripe javascript logic

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -3,17 +3,25 @@ class Payment < ApplicationRecord
   belongs_to :user
 
   def self.month_options
-    Date::MONTHNAMES.compact_each_with_index.map {|name, i| ["#{i+1} - #{name}", i+1]}
+    Date::MONTHNAMES.compact.each_with_index.map {|name, i| ["#{i+1} - #{name}", i+1]}
   end
 
   def self.year_options
     (Date.today.year..(Date.today.year+10)).to_a
   end
 
-  def process_payment
-    customer = Stripe::Customer.create email: email card: token
-    Stripe::Charge.create customer: customer.id, amount: 1000,
-                    description:'Premium',
-                    currency: 'usd'
+  def process_payment(email, token)
+    customer = Stripe::Customer.create(
+      email: email,
+      source: token
+    )
+  
+    Stripe::Charge.create(
+      customer: customer.id,
+      amount: 1000,
+      description: 'Premium',
+      currency: 'usd'
+    )
   end
+  
 end
